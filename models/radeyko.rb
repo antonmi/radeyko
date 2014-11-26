bc6353b0be9cb5c1452a547e4648533466980590
Anton Mishchuk <anton.mishchuk@onapp.com>
2014-11-26 11:53:27 +0200

    test_go

--- models/radeyko.rb
+++ models/radeyko.rb
@@ -1,16 +1,16 @@
 class Radeyko
 
   def self.start!
-    playlist = Playlist.from_dir("/home/antonmi/Dropbox/Music/Перкалаба")
+    playlist = Playlist.from_dir("/home/radeyko/music/Перкалаба")
     channel = Channel.new('test', playlist)
     channel.player.play
     Channel.channels.merge!(channel.name => channel)
 
-    100.times do |i|
-      playlist = Playlist.from_dir("/home/antonmi/Dropbox/Music/Перкалаба")
-      channel = Channel.new("test#{i}", playlist)
-      channel.player.play
-      Channel.channels.merge!(channel.name => channel)
-    end
+    # 100.times do |i|
+    #   playlist = Playlist.from_dir("/home/antonmi/Dropbox/Music/Перкалаба")
+    #   channel = Channel.new("test#{i}", playlist)
+    #   channel.player.play
+    #   Channel.channels.merge!(channel.name => channel)
+    # end
   end
 end
